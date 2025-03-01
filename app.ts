7f1d450f609bbad529bcae582785fbccac184ebb
Harry Odinson <vandet.dev@gmail.com>
2025-03-01 18:35:44 +0700

    update

--- app.ts
+++ app.ts
@@ -5,6 +5,8 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+app.use(express.static("public"));
+
 app.use(stream);
 
 app.listen(PORT, () => console.log("Server is running on port: " + PORT));
